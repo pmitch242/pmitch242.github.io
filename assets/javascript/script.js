fe322ed4140523a15838184ecc141e921a35b0fa
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2019-12-08 21:45:39 -0500

    created function for smooth scroll

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,21 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
+$(document).ready(function () {
+
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  });
+
+  // Smooth Scroll for all browsers
+  $(".navbar a").on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      },
+        800);
+    }
+  });
+  
 });
