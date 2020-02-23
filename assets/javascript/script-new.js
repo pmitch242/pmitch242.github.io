db68c537371237b5f44f597809b1397e18e56d00
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-23 11:03:28 -0500

    hide navbar

--- assets/javascript/script-new.js
+++ assets/javascript/script-new.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
     const introSection = $('#intro-section');
     const nextSection = $('#next-section');
     const optionSection = $('#option-section');
+    const navbar = $('#navbar');
 
     optionSection.hide();
+    navbar.hide();
 
     nextSection.on('click', function(){
         introSection.hide();
