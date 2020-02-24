e1fcf7e166867d6e2ece77364dee1d903b49458a
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-23 21:53:55 -0500

    done for the night

--- assets/javascript/script-new.js
+++ assets/javascript/script-new.js
@@ -1,15 +1,48 @@
 $(document).ready(function () {
     const introSection = $('#intro-section');
-    const nextSection = $('#next-section');
+    const nextSectionBtn = $('#next-section');
     const optionSection = $('#option-section');
     const navbar = $('#navbar');
+    const aboutBtn = $('.about-btn');
+    const portfolioBtn = $('.port-btn');
+    const aboutOptionDiv = $('#about-option');
+    const portfolioOptionDiv = $('#portfolio-option');
+    const aboutSection = $('#about-section');
+    const portfolioSection = $('#portfolio-section');
 
     optionSection.hide();
     navbar.hide();
 
-    nextSection.on('click', function(){
-        introSection.hide();
-        optionSection.show();
+    nextSectionBtn.on('click', function (event) {
+        event.preventDefault();
+        introSection.addClass('animated slideOutUp');
+        introSection.on('animationend', function () {
+            introSection.hide();
+            optionSection.show();
+            optionSection.addClass('animated slideInUp');
+        });
+    })
+
+    aboutBtn.on('click', function (event) {
+        event.preventDefault()
+        optionSection.css('background-color', '#f6f8ff')
+        portfolioOptionDiv.addClass('animated slideOutRight');
+        aboutOptionDiv.addClass('animated fadeOutRight')
+        portfolioOptionDiv.on('animationend', function () {
+            optionSection.hide();
+            aboutSection.show();
+        });
+    })
+
+    portfolioBtn.on('click', function (event) {
+        event.preventDefault()
+        optionSection.css('background-color', '#083d77')
+        aboutOptionDiv.addClass('animated slideOutLeft');
+        portfolioOptionDiv.addClass('animated fadeOutLeft')
+        aboutOptionDiv.on('animationend', function () {
+            optionSection.hide();
+            portfolioSection.show();
+        });
     })
 
 });
