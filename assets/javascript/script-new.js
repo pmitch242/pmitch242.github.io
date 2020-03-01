8c728f3ffb418e9fd2a52d3d80518c53eebe3d77
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-01 17:32:50 -0500

    check-point

--- assets/javascript/script-new.js
+++ assets/javascript/script-new.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
+    // =====================NOT USING!!!=====================
+    const navbar = $('#navbar');
+    const triviaBtn = $('#trivia-btn');
+    const triviaInfoDiv = $('#trivia-info-div');
+    const triviaImg = $('#trivia-img');
+    const cardCol = $(".card-col");
+    const cardHover = $('.card-hover');
+    // =======================================================
+
+    // =====================USING!!!!!!!!=====================
     const introSection = $('#intro-section');
     const optionSection = $('#option-section');
-    const navbar = $('#navbar');
     const aboutBtn = $('.about-btn');
     const portfolioBtn = $('.port-btn');
     const aboutOptionDiv = $('#about-option');
@@ -9,22 +18,23 @@ $(document).ready(function () {
     const aboutSection = $('#about-section');
     const portfolioSection = $('#portfolio-section');
     const card = $('.card');
+    const exitBtn = $('.exit-btn');
     const backgroundHalf = $('.background-half');
     const introDiv = $('.intro-div')
     const introNextBtn = $('#intro-next');
-    const iconLinkDiv = $('.icon-link-div');
-    const triviaBtn = $('#trivia-btn');
-    const triviaInfoDiv = $('#trivia-info-div');
-    const triviaImg = $('#trivia-img');
-    const cardCol = $(".card-col");
-    const cardHover = $('.card-hover');
-    const exitBtn = $('.exit-btn');
+    const switchOption = $('.switch-btn');
+    const switchH3 = $('.switch-h3');
+    const switchInfo = $('.switch-info');
+    const switchArrow = $('.switch-arrow');
+    // =======================================================
 
-    portfolioSection.hide();
-    backgroundHalf.hide();
-    optionSection.hide();
+
+    // portfolioSection.hide();
+    // backgroundHalf.hide();
+    // optionSection.hide();
+    applyCardHover();
+    applySwitchHover();
     landingPageStart();
-    applyHover();
 
     introNextBtn.on('click', function (event) {
         event.preventDefault();
@@ -34,7 +44,7 @@ $(document).ready(function () {
             optionSection.show();
             optionSection.addClass('animated slideInUp');
         });
-    })
+    });
 
     aboutBtn.on('click', function (event) {
         event.preventDefault()
@@ -43,27 +53,33 @@ $(document).ready(function () {
         aboutOptionDiv.addClass('animated fadeOutRight')
         portfolioOptionDiv.on('animationend', function () {
             optionSection.hide();
-            aboutSection.show();
+            aboutSection.css('display', 'flex');
         });
-    })
+    });
 
     portfolioBtn.on('click', function (event) {
-        event.preventDefault()
+        event.preventDefault();
+        switchOption.show();
         $('body').css('background-image', 'none');
         $('body').css('background-color', '#083d77');
         aboutOptionDiv.addClass('animated slideOutLeft');
         portfolioOptionDiv.addClass('animated fadeOutLeft');
         aboutOptionDiv.on('animationend', function () {
             optionSection.hide();
-            portfolioSection.show();
+            portfolioSection.css('display', 'flex');
             portfolioSection.addClass('animated fadeInRight')
+            portfolioSection.on('animationend', function () {
+                switchH3.addClass('animated fadeIn');
+                switchH3.show();
+                switchOption.css('box-shadow', '4px 4px 8px 0 rgba(0, 0, 0, .4)');
+            });
         });
-    })
+    });
 
 
     // Card effects
 
-    function applyHover() {
+    function applyCardHover() {
         card.hover(
             function () {
                 let cardDataId = $(this).attr('data-id');
@@ -99,6 +115,87 @@ $(document).ready(function () {
             }
         )
     };
+
+    function applySwitchHover() {
+        switchOption.mouseenter(function () {
+            switchInfo.show();
+            switchArrow.show();
+            switchOption.css('cursor', 'pointer');
+            switchOption.animate({ width: '75px' });
+            switchH3.animate({ 
+                left: '-45px',
+                transform: 'scale(1.1)'
+        });
+            switchInfo.addClass('animated fadeIn');
+            switchInfo.animate({ left: '30px' });
+            switchArrow.addClass('animated fadeIn');
+            switchArrow.animate({ left: '52px' });
+        })
+        switchOption.mouseleave(function () {
+            switchInfo.animate({
+                left: '25px',
+                opacity: 0,
+            }, 500, function () {
+                switchInfo.css('left', '25px');
+                switchInfo.hide();
+            });
+            switchArrow.animate({
+                left: '42px',
+                opacity: 0,
+            }, 500, function () {
+                switchArrow.css('left', '42px');
+                switchArrow.hide();
+            })
+            switchOption.animate({ width: '60px' });
+            switchH3.animate({
+                left: '-52px',
+                transform: 'scale(1)'
+            });
+            switchArrow.removeClass('animated fadeIn faster');
+            switchInfo.removeClass('animated fadeIn faster');
+        });
+        // switchOption.hover(
+        //     function () {
+        //         switchOption.css('cursor', 'pointer');
+        //         switchOption.animate({ width: '75px' });
+        //         switchH3.animate({ left: '-45px' });
+        //         // switchInfo.addClass('animated fadeIn faster');
+        //         // switchInfo.show();
+        //         // switchInfo.animate({ left: '30px' });
+        //         // switchArrow.addClass('animated fadeIn faster');
+        //         // switchArrow.show();
+        //         // switchArrow.animate({ left: '52px' });
+        //     },
+        //     function () {
+        //         switchArrow.hide();
+        //         switchArrow.removeClass('animated fadeOut');
+        //         switchInfo.removeClass('animated');
+        //         resetSwitch();
+        //         // switchOption.animate({ width: '60px' });
+        //         // switchH3.animate({ left: '-52px' });
+        //         // switchArrow.animate({ left: '42px' });
+        //         // switchInfo.animate({
+        //         //     left: '25px',
+        //         //     opacity: 0,
+        //         //     // fadeOut()
+        //         // }, 500, function () {
+        //         //     switchInfo.css('left', '25px')
+        //         // });
+        //         applySwitchHover();
+        //         // switchInfo.fadeOut();
+        //         // switchOption.css('width', '60px');
+        //         // switchH3.css('left', '-52px');
+        //     }
+        // );
+        // resetSwitch();
+        // applySwitchHover();
+    };
+
+    function resetSwitch() {
+        switchOption.animate({ width: '60px' });
+        switchH3.animate({ left: '-52px' });
+        switchArrow.animate({ left: '42px' });
+    }
 
     card.on('click', function () {
         let thisCard = $(this);
@@ -148,7 +245,7 @@ $(document).ready(function () {
         let exitBtnDataId = $(this).attr('data-id');
         card.removeClass('card-active card-inactive');
         // card.removeClass('card-inactive');
-        applyHover();
+        applyCardHover();
         $('.info-div').each(function () {
             if ($(this).attr('data-id') === exitBtnDataId + 'a') {
                 let infoDiv = $(this);
@@ -157,11 +254,12 @@ $(document).ready(function () {
                 infoDiv.removeClass('animated fadeOut div-active flipInX');
             }
         })
-    })
+    });
 
     function landingPageStart() {
-        backgroundHalf.show();
         backgroundHalf.addClass('animated slideInRight');
+        // backgroundHalf.animate().show();
+        backgroundHalf.show();
         backgroundHalf.on('animationend', function () {
             introDiv.css('visibility', 'visible');
             introDiv.addClass('animated slideInLeft');
@@ -175,6 +273,6 @@ $(document).ready(function () {
             })
 
         })
-    }
+    };
 
 });
