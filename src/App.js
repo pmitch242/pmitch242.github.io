5466c31bfda1ba2564332169b919e2da18146c8b
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-03-27 09:51:58 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <h1>Testing</h1>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path={['/']} />
+          <Route exact path={['/about']}/>
+          <Route exact path={['/portfolio']}/>
+          <Route >
+            <h1>No page found</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
