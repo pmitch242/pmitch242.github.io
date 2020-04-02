366f54fa920e487bec1d9968752df096ff19ea5c
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-02 01:24:21 -0400

    making portfolio page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { Container, Col, Row } from 'react-bootstrap'
-import About from '../../components/About'
+import { Container, Col, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Portfolio from '../../components/Portfolio';
+import About from '../../components/About';
 
 export default class Profile extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col>1 of 2</Col>
-                    <Col><About /></Col>
-                </Row>
-            </Container>
+            <Router>
+                <Container>
+                    <Row>
+                        <Col>1 of 2</Col>
+                        <Col>
+                        <Switch>
+                            <Route path='/about'><About /></Route>
+                            <Route path='/profolio'><Portfolio /></Route>
+                        </Switch>                            
+                        </Col>
+                    </Row>
+                </Container>
+            </Router>
+
         )
     }
 }
