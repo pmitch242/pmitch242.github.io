f6766c4b38401ae1bc87e6a755694f62a655118d
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-03 04:07:04 -0400

    set up project card

---
+++ src/components/ProjectCards/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './style.css'
+import { Card } from 'react-bootstrap';
+
+
+
+export default class ProjectCard extends Component {
+    render() {
+        return (
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title className="mb-2 text-muted">this.props.name</Card.Title>
+                    <img alt='project' src={this.props.image}/>
+                    <Card.Text>
+                        {this.props.prject}.
+                    </Card.Text>
+                    <Card.Link href={this.props.deployed}>Deployed Link</Card.Link>
+                    <Card.Link href={this.props.github}>GitHub Link</Card.Link>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
