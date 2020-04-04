0b84192e03db54a7338b35365f387ad5b22feec0
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-04 01:17:29 -0400

    fixed rediect bug and added styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,18 +7,18 @@ import './style.css'
 export default class About extends Component {
     constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             redirect: null,
         }
     }
 
-    handleSubmit = () =>{
+    handleSubmit = () => {
         this.setState({ redirect: true });
     }
 
     handleRedirect = () => {
-        if(this.state.redirect){
-            return <Redirect to ='/portfolio'/>
+        if (this.state.redirect) {
+            return <Redirect to='/portfolio' />
         }
     }
 
@@ -31,12 +31,20 @@ export default class About extends Component {
                 <div className='about'>
                     <h1>Full Stack Developer</h1>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum dictum risus,
-                        a scelerisque tortor ultricies non. Aenean efficitur porttitor dui ut ullamcorper.
-                        In ac cursus turpis. Praesent sed orci molestie diam pretium scelerisque. Mauris sit amet felis lobortis,
-                        rhoncus mi ac, semper urna. Aenean non cursus ipsum. Phasellus ac dolor malesuada,
-                        dapibus diam id, maximus dolor. Duis nulla est, scelerisque non pretium eget, pharetra vel sapien.
-                        Donec eu consequat nibh. Nullam in est in tortor dapibus ultrices id in orci.
+                        Hi! I’m Phill (they/them), a full-stack developer living in Baltimore, MD. Originally from The
+                        Bahamas, I moved to the states in 2014 to attend college at Minnesota State University,
+                        Mankato, where I earned a dual bachelor’s degree in Computer Engineering Technology and
+                        Electronic Engineering Technology. After college, I worked as a Controls Systems Specialist and
+                        an R&amp;D Engineer, before realizing that I needed more of a challenge in my work. Having
+                        recently completed a 6-month full-stack certification program through Johns Hopkins
+                        University, I’m excited to begin the next stage of my career.
+                    </p>
+                    <p>
+                        Despite the long hours (and occasional fits of screaming at my computer) that come with being
+                        a developer, I’ve never enjoyed my work as thoroughly as I do when I’m coding. Full-stack
+                        engineering requires me to think creatively and analytically about problem-solving. I believe
+                        everything can always be improved, and I’m drawn to the nuance and attention to detail
+                        required to excel as a full-stack developer.
                     </p>
                     <Button onClick={this.handleSubmit} variant="outline-dark">PORTFOLIO</Button>
                 </div>
