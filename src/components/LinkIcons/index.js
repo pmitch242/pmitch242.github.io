3d1e95cec0388e7b54acd34d62d4c080120b4f2f
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-10 15:38:43 -0400

    fixed pdf bug

--- src/components/LinkIcons/index.js
+++ src/components/LinkIcons/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import resume from './resume.pdf'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelopeOpen as faEnvelopeOpenRegular, faFile } from '@fortawesome/free-regular-svg-icons'
 import { faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-icons'
@@ -7,9 +8,9 @@ import './style.css'
 export default class LinksIcon extends Component{
     render(){
         return <ul className='icon-div'>
-            <li><a href='mailto:phill.mitchell21@gmail.com'><FontAwesomeIcon icon={ faEnvelopeOpenRegular } /></a></li>
+            <li><a href='mailto:phill.mitchell211@gmail.com'><FontAwesomeIcon icon={ faEnvelopeOpenRegular } /></a></li>
             <li><a href='https://www.linkedin.com/in/phillicia-mitchell-matsuyama21/'><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
-            <li><a href='./resume.pdf'><FontAwesomeIcon icon={faFile} /></a></li>
+            <li><a href={ resume } target="_blank"><FontAwesomeIcon icon={faFile} /></a></li>
             <li><a href='https://github.com/pmitch242'><FontAwesomeIcon icon={faGithub} /></a></li>
         </ul>
     }
