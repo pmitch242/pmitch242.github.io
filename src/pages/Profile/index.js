1dfb3b5c27ffbff545550eef0ee1df2b8bb8461f
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-10 13:04:00 -0400

    removed router

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Portfolio from '../Portfolio';
 import About from '../About';
 import './style.css'
@@ -8,21 +7,16 @@ import './style.css'
 export default class Profile extends Component {
     render() {
         return (
-            <Router>
-                <Container fluid className='profile-container'>
-                    <Row>
-                        <Col lg={8} className='first-half'>
-                            <Portfolio />
-                        </Col>
-                        <Col xs>
-                            <Switch>
-                                <About />
-                            </Switch>
-                        </Col>
-                    </Row>
-                </Container>
-            </Router>
-
+            <Container fluid className='profile-container'>
+                <Row>
+                    <Col lg={8} className='first-half'>
+                        <Portfolio />
+                    </Col>
+                    <Col xs>
+                        <About />
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
