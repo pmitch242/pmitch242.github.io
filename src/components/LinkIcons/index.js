2bf4678a07e809d74b5a916e10ac1b14bc5c1ed0
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-11 08:16:57 -0400

    added class names for each link

--- src/components/LinkIcons/index.js
+++ src/components/LinkIcons/index.js
@@ -8,10 +8,10 @@ import './style.css'
 export default class LinksIcon extends Component{
     render(){
         return <ul className='icon-div'>
-            <li><a href='mailto:phill.mitchell211@gmail.com'><FontAwesomeIcon icon={ faEnvelopeOpenRegular } /></a></li>
-            <li><a href='https://www.linkedin.com/in/phillicia-mitchell-matsuyama21/'><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
-            <li><a href={ resume } ><FontAwesomeIcon icon={faFile} /></a></li>
-            <li><a href='https://github.com/pmitch242'><FontAwesomeIcon icon={faGithub} /></a></li>
+            <li><a className='email-icon' href='mailto:phill.mitchell211@gmail.com'><FontAwesomeIcon icon={ faEnvelopeOpenRegular } /></a></li>
+            <li><a className='linkedIn-icon' href='https://www.linkedin.com/in/phillicia-mitchell-matsuyama21/'><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
+            <li><a className='resume-icon' href={ resume } ><FontAwesomeIcon icon={faFile} /></a></li>
+            <li><a className='github-icon' href='https://github.com/pmitch242'><FontAwesomeIcon icon={faGithub} /></a></li>
         </ul>
     }
 }
