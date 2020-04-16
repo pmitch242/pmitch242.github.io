d2bf033b68183d95a5dce5a56685a2158b32cb95
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-04-16 12:06:02 -0400

    updated projects and project cards

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './style.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Card } from 'react-bootstrap';
 
 
@@ -12,15 +13,8 @@ export default class ProjectCard extends Component {
     render() {
         return (
             <Card className='project-card'>
-                <Card.Body>
-                    <Card.Title className="mb-2 text-muted">{this.props.name}</Card.Title>
-                    <img className='project-card-image' alt='project' src={this.props.image}/>
-                    <Card.Text>
-                        {this.props.description}.
-                    </Card.Text>
-                    <Card.Link href={this.props.github}>GitHub Link</Card.Link>
-                    <Card.Link href={this.props.deployed}>Deployed Link</Card.Link>
-                </Card.Body>
+                   { this.props.icon }
+                    <h1 className="mb-2 text-muted">{this.props.name}</h1>
             </Card>
         )
     }
