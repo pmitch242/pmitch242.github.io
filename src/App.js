8e7beaeab07b650d3186e1f27be90ca7c8f4cdcb
Phill <phillicia.mitchell21@gmail.com>
2020-05-06 16:39:11 -0400

    moved navbar to app.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Portfolio from './pages/Portfolio'
+import Navbar  from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div>
-      <BreakpointProvider>
+        <BreakpointProvider>
 
-        <Switch>
+          <Switch>
 
-          <Route exact path={'/'} ><Home /></Route>
-            <Route exact path={'/profile'}><Profile /></Route>
-          <Route exact path={'/portfolio'}><Portfolio /></Route>
-          <Route >
-            <h1>No page found</h1>
-          </Route>
-        </Switch>
+            <Route exact path={'/'} ><Home /></Route>
+            <Route exact path={'/profile'}>
+              <Navbar />
+              <Profile />
+            </Route>
+            <Route exact path={'/portfolio'}>
+              <Navbar />
+              <Portfolio />
+            </Route>
+            <Route >
+              <h1>No page found</h1>
+            </Route>
+          </Switch>
         </BreakpointProvider>
       </div>
     </Router>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import Portfolio from '../Portfolio';
 import About from '../About';
-import Navbar from '../../components/Navbar';
 import './style.css'
 import { Breakpoint } from 'react-socks';
 
@@ -10,7 +9,6 @@ export default class Profile extends Component {
     render() {
         return (
             <Container fluid className='profile-container'>
-                <Navbar/>
                 <Row>
                     <Col lg={8} className='first-half'>
                         <Breakpoint customQuery="(min-width: 992px)">
