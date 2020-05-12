01bcc460828d9c94e29783860e7aabd0ca448ef3
Phill <phillicia.mitchell21@gmail.com>
2020-05-12 12:43:38 -0400

    check point

---
+++ src/components/DesktopPortfolio/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import NeuBtn from '../NeuBtn';
+import { Card } from 'react-bootstrap';
+
+import yappeDesktopImage from './images/yappe-desktop.png';
+import yappeMobileImage from './images/yappe-mobile.png';
+// import triviaDesktopImage from './images/trivia-desktop.PNG';
+// import triviaMobileImage from './images/trivia-mobile.png';
+// import googleDesktopImage from './images/google-desktop.PNG';
+
+import './style.css';
+
+export default function DesktopPortfolio(props) {
+    return (
+        <Card className='desktop-portfolio'>
+            <Card.Body>
+                <Card.Title>Desktop Portfolio</Card.Title>
+
+                <div className='desktop-portfolio-image-div'>
+                    <div className='desktop-img-div'>
+                        <img className='desktop-img' alt='Desktop View' src={yappeDesktopImage}></img>
+                    </div>
+                    <div className='mobile-img-div'>
+                        <img className='mobile-img' style={{width: '100%'}} alt='Mobile View' src={yappeMobileImage}></img>
+                    </div>
+                </div>
+
+                <div className='desktop-portfolio-btns'>
+                    <NeuBtn
+                        className='test'
+                        hover='yappe-color-hover'
+                        name='Website'
+                        link='https://yappeapp.herokuapp.com/'
+                    />
+                    <NeuBtn
+                        hover='yappe-color-hover'
+                        name='GitHub'
+                        link='https://github.com/anzook/yappe-app'
+                    />
+                </div>
+            </Card.Body>
+        </Card>
+    )
+}
+
+
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Breakpoint } from 'react-socks';
 import { Container, Col, Row } from 'react-bootstrap';
-import Navbar from '../../components/Navbar'
+import Navbar from '../../components/Navbar';
+import DesktopPortfolio from '../../components/DesktopPortfolio';
 import Portfolio from '../Portfolio';
 import About from '../About';
 
@@ -12,20 +13,46 @@ export default class Profile extends Component {
     render() {
         return (
             <Container fluid className='profile-container'>
-                <Breakpoint customQuery="(max-width: 992px)">
-                    <Navbar />
-                </Breakpoint>
-
-                <Row>
-                    <Col lg={8} className='first-half'>
-                        <Breakpoint customQuery="(min-width: 992px)">
-                            <Portfolio desktop={true}/>
-                        </Breakpoint>
-                    </Col>
-                    <Col >
+                {/* <Breakpoint customQuery="(max-width: 992px)"> */}
+                {/* <Navbar /> */}
+                <Row className='profile-row'>
+                    {/* <Col >
                         <About />
+                    </Col> */}
+
+                    {/*  */}
+                    <Col >
+                        <Breakpoint customQuery="(max-width: 1004px)">
+                            <About small={true} />
+                        </Breakpoint>
+
+                        <Breakpoint customQuery="(min-width: 1005px)">
+                            <About small={false} />
+                        </Breakpoint>
+
                     </Col>
+
+
+                    <Col lg={8} className='first-half'>
+                        <DesktopPortfolio />
+                        <DesktopPortfolio />
+                    </Col>
+
                 </Row>
+
+                {/* <Breakpoint customQuery="(min-width: 992px)">
+                    <Row>
+                        <Col lg={8} className='first-half'>
+                            <Breakpoint customQuery="(min-width: 992px)">
+                                <DesktopPortfolio desktop={true} />
+                                <DesktopPortfolio desktop={true} />
+                            </Breakpoint>
+                        </Col>
+                        <Col >
+                            <About />
+                        </Col>
+                    </Row>
+                </Breakpoint> */}
             </Container>
         )
     }
