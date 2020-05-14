ccc500637b9d4af2939a72959e5165a1965f13e8
Phill <phillicia.mitchell21@gmail.com>
2020-05-14 13:43:49 -0400

    add classes for page buttons

--- src/components/ProjectSwitch/index.js
+++ src/components/ProjectSwitch/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import $ from "jquery";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,11 +8,13 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import './style.css'
 
 export default function ProjectSwitch(props) {
+
+
     return (
         <ul className={`project-select-ul ${props.large ? '' : 'hide'}`}>
-            <li><FontAwesomeIcon icon={faCircle} /></li>
-            <li><FontAwesomeIcon icon={faCircle} /></li>
-            <li><FontAwesomeIcon icon={faCircle} /></li>
+            <li className='dot yappeDot'><FontAwesomeIcon icon={faCircle} /></li>
+            <li className='dot triviaDot'><FontAwesomeIcon icon={faCircle} /></li>
+            <li className='dot googleDot'><FontAwesomeIcon icon={faCircle} /></li>
         </ul>
     )
 }
