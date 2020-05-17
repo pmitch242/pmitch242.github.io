f2b3451f05e3996cebb20cf7fc2a4092fdda0ada
Phill <phillicia.mitchell21@gmail.com>
2020-05-17 15:26:12 -0400

    added styled active project icon

--- src/components/ProjectSwitch/index.js
+++ src/components/ProjectSwitch/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import $ from "jquery";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faPaw, faGamepad, faBookOpen } from '@fortawesome/free-solid-svg-icons';
+
 
 
 import './style.css'
 
 export default function ProjectSwitch(props) {
+    const [select, setSelect] = useState('yappe')
     // const scrollingDiv = $('body');
     // const yappeContainer = $('.profile-row');
     // const triviaContainer = $('#desktop-trivia-div');
@@ -17,18 +19,21 @@ export default function ProjectSwitch(props) {
 
         switch (dot) {
             case 'trivia':
+                setSelect('trivia');
                 $('html, body').animate({
                     scrollTop: $("#desktop-trivia-div").offset().top
                 }, 750);
                 break;
 
             case 'google':
+                setSelect('google');
                 $('html, body').animate({
                     scrollTop: $("#desktop-google-div").offset().top
                 }, 750);
                 break;
 
             default:
+                setSelect('yappe');
                 $('html, body').animate({
                     scrollTop: $("#desktop-yappe-div").offset().top
                 }, 750);
@@ -38,26 +43,38 @@ export default function ProjectSwitch(props) {
 
     return (
         <ul className={`project-select-ul ${props.large ? '' : 'hide'}`}>
-            <li className='dot yappe-dot'
+            <li className='yappe-li'
                 onClick={() => {
                     scroll('yappe');
                     props.action('yappe');
                 }}>
-                <FontAwesomeIcon icon={faCircle} />
+                {select !== 'yappe' &&
+                    <FontAwesomeIcon className='yappe-dot' icon={faCircle} />}
+                {select === 'yappe' &&
+                    <FontAwesomeIcon className='active-icon-switch' icon={faPaw} />}
             </li>
-            <li className='dot trivia-dot'
+            <li className='trivia-li'
                 onClick={() => {
                     scroll('trivia');
                     props.action('trivia');
                 }}>
-                <FontAwesomeIcon icon={faCircle} />
+                {select !== 'trivia' &&
+                    <FontAwesomeIcon className='trivia-dot' icon={faCircle} />}
+
+                {select === 'trivia' &&
+                    <FontAwesomeIcon className='active-icon-switch' icon={faGamepad} />}
             </li>
-            <li className='dot google-dot'
+            <li className='google-li'
                 onClick={() => {
                     scroll('google');
                     props.action('google');
                 }}>
-                <FontAwesomeIcon icon={faCircle} />
+
+                {select !== 'google' &&
+                    <FontAwesomeIcon className='google-dot' icon={faCircle} />}
+
+                {select === 'google' &&
+                    <FontAwesomeIcon className='active-icon-switch' icon={faBookOpen} />}
             </li>
         </ul>
     )
